acd8148926881e2a9abdf5b32998090627421f10
PGSStas <stas.katsuba@gmail.com>
2019-05-18 19:53:30 +0300

    SaveChanges

--- scripts/healthBar.js
+++ scripts/healthBar.js
@@ -1,23 +1,22 @@
+var hp = 200;
+var fullHP = hp;
+var damage = 1;
 
-	var hp = 200;
-	var fullHP = hp;
-	var damage = 1;
+function reduceHP () {
+	if (hp <= 0) {
+		alert("Already Dead");
+    } else {
+    	hp = Math.max(0, hp - damage);
+		let width = (hp / fullHP * 100); 
+		document.getElementById("healthBar").style.width = Math.max(width, 0.0) + '%'; 
+  	}
+};
 
-	function reduceHP () {
-		if (hp <= 0) {
-			alert("Already Dead");
-	    } else {
-	    	hp = Math.max(0, hp - damage);
-			let width = (hp / fullHP * 100); 
-			document.getElementById("healthBar").style.width = Math.max(width, 0.0) + '%'; 
-	  	}
-	};
-
-	function resetHP(){
-		document.getElementById("healthBar").style.width = '100%';
-		hp = document.getElementById("HP").value;
-		damage = document.getElementById("damage").value;
-		fullHP = hp;
-	};
+function resetHP(){
+	document.getElementById("healthBar").style.width = '100%';
+	hp = document.getElementById("HP").value;
+	damage = document.getElementById("damage").value;
+	fullHP = hp;
+};
 
 
