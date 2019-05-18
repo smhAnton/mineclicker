0279631bc4011bd90a17870e12db16f3a5005e50
PGSStas <stas.katsuba@gmail.com>
2019-05-18 16:01:37 +0300

    Added HealthBar

---
+++ scripts/healthBar.js
@@ -0,0 +1,20 @@
+var hp = 3;
+var fullHP = hp;
+var damage = 3;
+
+function reduceHP () {
+	if (hp <= 0) {
+		alert("Already Dead");
+    } else {
+    	hp = Math.max(0, hp - damage);
+		let width = (hp / fullHP * 100); 
+		currentHealth.style.width = Math.max(width, 0.0) + '%'; 
+  }
+};
+
+function resetHP(){
+	document.getElementById("healthBar").style.width = '100%';
+	hp = document.getElementById("HP").value;
+	damage = document.getElementById("damage").value;
+	fullHP = hp;
+};
