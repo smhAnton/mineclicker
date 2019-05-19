035ac212ea5b82f2e6b825d9864f3896f3331ecb
PGSStas <stas.katsuba@gmail.com>
2019-05-19 14:47:17 +0300

    Json fixed

--- scripts/healthBar.js
+++ scripts/healthBar.js
@@ -1,23 +1,29 @@
-window.onload = function (){
-	var hp = 3;
-	var fullHP = hp;
-	var damage = 3;
+var hp = 1000;
+var fullHP = hp;
+var damage = 3;
+var bioms;
 
-	function reduceHP () {
-		if (hp <= 0) {
-			alert("Already Dead");
-	    } else {
-	    	hp = Math.max(0, hp - damage);
-			let width = (hp / fullHP * 100); 
-			currentHealth.style.width = Math.max(width, 0.0) + '%'; 
-	  }
-	};
+function load(){
+	bioms = JSON.parse(data);
+}
 
-	function resetHP(){
-		document.getElementById("healthBar").style.width = '100%';
-		hp = document.getElementById("HP").value;
-		damage = document.getElementById("damage").value;
-		fullHP = hp;
-	};
+function reduceHP () {
+	if (hp <= 0) {
+		alert("Already Dead");
+    } else {
+    	hp = Math.max(0, hp - damage);
+		let width = (hp / fullHP * 100); 
+		document.getElementById("healthBar").style.width = Math.max(width, 0.0) + '%';
+  }
+  
 };
+
+function resetHP(){
+	document.getElementById("healthBar").style.width = '100%';
+	hp = document.getElementById("HP").value;
+	damage = document.getElementById("damage").value;
+	fullHP = hp;
+
+};
+
 
